469298b0ec88e34bd8da135097198a1bad4c45c1
Shikhar Dahal <49984957+shikhardahal@users.noreply.github.com>
2023-10-18 22:17:36 +0545

    Activity page design

--- src/app/activities/page.tsx
+++ src/app/activities/page.tsx
@@ -1,33 +1,40 @@
-import React, { useState, useEffect } from "react";
-import style from "./_style.module.scss";
-import ActivitiesCard from '../../components/card/ActivitiesCard/index.tsx';
-import { useClient } from 'next/react-server';
+import React from "react";
 
-useClient();
+import style from "./_style.module.scss";
+import ActivitiesCard from "@/components/Card/ActivitesCard";
+import Pagination from "@/components/Pagination";
+
+const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 const ActivitiesPage = () => {
-  const [activities, setActivities] = useState([]);
-
-  useEffect(() => {
-    const fetchActivities = async () => {
-      try {
-        const response = await fetch("http://localhost:1337/api/footer-contents?populate=AboutAnsdd");
-        const data = await response.json();
-        setActivities(data);
-      } catch (error) {
-        console.error("Error fetching activities:", error);
-      }
-    };
-
-    fetchActivities();
-  }, []);
-
   return (
-    <section className={style.grid_responsive}>
-      {activities?.map((activity, index) => (
-        <ActivitiesCard key={index} activity={activity} />
-      ))}
-    </section>
+    <article className={style.article_page}>
+      <section className={style.article_header}>
+        <h2 className={style.article_title}>Activities</h2>
+        <p>Home / Activities</p>
+      </section>
+      <div className={style.article_menu}>
+        <button className="btn btn-primary">Past Activities</button>
+        <button className="btn btn-secondary">Upcoming Activities</button>
+      </div>
+      <section className={style.container}>
+        <div className={style.grid_responsive}>
+          {number?.map((item, index) => (
+            <ActivitiesCard key={index} />
+          ))}
+        </div>
+      </section>
+      <Pagination />
+      <div className={style.article_footer}>
+        <section className={`${style.container} ${style.footer_container}`}>
+          <h2 className={style.article_footer_title}>
+            Get all the latest news, updates and documents delivered directly to
+            your inbox instantly
+          </h2>
+          <button className="btn btn-primary">Subscribe to ANSSD</button>
+        </section>
+      </div>
+    </article>
   );
 };
 
