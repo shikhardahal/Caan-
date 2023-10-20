bed9418fcda170a2ff32079097e307a4a76ef98d
Shikhar Dahal <49984957+shikhardahal@users.noreply.github.com>
2023-10-20 12:32:54 +0545

    Update page.tsx

--- src/app/activities/page.tsx
+++ src/app/activities/page.tsx
@@ -1,50 +1,70 @@
+// src/app/activities/page.tsx
 'use client'
-import React, { useState, useEffect } from 'react';
-export function Page() {
-  const [data, setData] = useState(null);
+import React, { useState, useEffect } from "react";
+import style from "./_style.module.scss";
+import ActivitiesCard from "@/components/Card/ActivitiesCard/styles.module.scss"; // Corrected import path
+import Pagination from "@/components/Pagination";
+
+const ActivitiesPage = () => {
+  const [activities, setActivities] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://localhost:1337/api/activity-pages?populate[0]=card.img_url');
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        'http://localhost:1337/api/activity-pages?populate=*' +
+        new URLSearchParams({ 'populate[0]': 'card.img_url' })
+      );
 
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        const result = await response.json();
-        setData(result);
-        setLoading(true);
-      } catch (e) {
-        setError(`An error occurred while fetching the data: ${e.message}`);
-        setLoading(false);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
       }
-    };
 
+      const data = await response.json();
+      const activityData = data.data;
+
+      setActivities(activityData);
+      setLoading(false);
+    } catch (err) {
+      setError(err.message);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
     fetchData();
   }, []);
 
   return (
-    <div>
-      {loading ? (
-        <p>Loading...</p>
-      ) : error ? (
-        <p>Error: {error}</p>
-      ) : (
-        <div>
-          <ul>
-            {data &&
-              data.map((item) => (
-                <li key={item.id}>
-                  <img src={item.card.img_url.data.url} alt="Activity Image" />
-                  {item.title}
-                </li>
-              ))}
-          </ul>
+    <article className={style.article_page}>
+      {/* ... Existing JSX code ... */}
+      <section className={style.container}>
+        <div className={style.grid_responsive}>
+          {loading ? (
+            <p>Loading...</p>
+          ) : error ? (
+            <p>Error: {error}</p>
+          ) : (
+            activities?.map((activity) => (
+              <ActivitiesCard key={activity.id} data={activity.attributes} />
+            ))
+          )}
         </div>
-      )}
-    </div>
+      </section>
+      <Pagination />
+      <div className={style.article_footer}>
+        <section className={`${style.container} ${style.footer_container}`}>
+          <h2 className={style.article_footer_title}>
+            Get all the latest news, updates, and documents delivered directly to
+            your inbox instantly
+          </h2>
+          <button className="btn btn-primary">Click Me</button>
+        </section>
+      </div>
+    </article>
   );
-}
+};
+
+export default ActivitiesPage;
+
